930c48703aab64782c637431eb1ed1d683eeaab3
gorod-old <gorod1974@hotmail.com>
2020-01-18 19:09:44 +0300

    commit 1

---
+++ GitHubTest/Assets/Scripts/GeneralScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralScript : MonoBehaviour
+{
+    public Text info;
+
+    /// <summary>
+    /// 3000 lines of code in which I'm tired of looking for the methods I need))
+    /// </summary>
+
+    public void BtClick()
+    {
+        info.text = "My first version that I commit to github repository.";
+    }
+}
